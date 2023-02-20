55754251d4073e8c01e18ae5753fc87ae45662b8
Tanya <itsmetanya@mail.ru>
2023-02-20 17:28:31 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход приримает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int number = new Random().Next(0, 100); 
+Console.WriteLine(number);
+int numbereven = 2;
+if(number > 1)
+{
+    while(numbereven <= number)
+    {Console.Write(numbereven + ", ");
+    numbereven = numbereven + 2;
+    }
+} 
+
