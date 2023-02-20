a2b2f942ae0e210fc6ec394a641de5227d302ea2
Tanya <itsmetanya@mail.ru>
2023-02-20 16:49:42 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа , которая на вход принимает число и выдаёт, является ли число чётным.
+
+int number = new Random().Next(2, 1000); 
+Console.WriteLine(number);
+
+if(number == number % 2);
+{
+    Console.Write("чётное");
+}
