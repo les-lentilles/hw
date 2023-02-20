fdf6fd2b1d9cfb29831f3136ccf91d0ee6ae6dcb
Tanya <itsmetanya@mail.ru>
2023-02-20 16:08:58 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход 3 числа и выдаёт максимальное из этих чисел:
+
+int a = 10;
+int b = 25;
+int c = 46;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
